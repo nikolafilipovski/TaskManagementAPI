162f86f0b186a9d6d307ba55dba33fec0d46b162
Filipovski <Filipovski@DESKTOP-V9E4O51>
2024-11-29 22:30:54 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystemData.Entities;
+using TaskManagementSystemService.Interfaces;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+
+        public AuthController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public ActionResult<LoginResponse> Login([FromBody] UserDto user)
+        {
+            if (user.Username == "admin" && user.Password == "password123")
+            {
+                var token = _tokenService.GenerateToken(user.Username);
+                var response = new LoginResponse
+                {
+                    Token = token
+                };
+                return Ok(response); // Return the generated token
+            }
+
+            return Unauthorized("Invalid credentials");
+        }
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ Controllers/TaskController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystemData.Entities;
+using TaskManagementSystemService.Interfaces;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITasksService _tasksService;
+
+        public TaskController(ITasksService tasksService)
+        {
+            _tasksService = tasksService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Task>>> GetAllTasks()
+        {
+            var tasks = await _tasksService.GetAllTasks();
+
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Task>> CreateTask(TaskDto task)
+        {
+            var result = await _tasksService.CreateTask(task);
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Task>> UpdateTask(TaskDto task)
+        {
+            var updatedTask = await _tasksService.UpdateTask(task);
+
+            if (updatedTask == null)
+                return NotFound();
+
+            return Ok(updatedTask);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteTask(int taskId)
+        {
+            var success = await _tasksService.DeleteTask(taskId);
+
+            if (!success)
+                return NotFound();
+
+            return Ok(true);
+        }
+    }
+}
