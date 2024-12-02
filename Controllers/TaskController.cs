c2fedea35db4468e150a8e0f91d9ac0029480e02
Nikola Filipovski <76019543+nikolafilipovski@users.noreply.github.com>
2024-12-02 14:46:01 +0100

    .

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystemData.Entities;
+using TaskManagementSystemService.Interfaces;
+
+namespace TaskManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITasksService _tasksService;
+
+        public TaskController(ITasksService tasksService)
+        {
+            _tasksService = tasksService;
+        }
+
+        [Authorize]
+        [HttpGet("GetAllTasks")]
+        public async Task<ActionResult<IEnumerable<Task>>> GetAllTasks()
+        {
+            var tasks = await _tasksService.GetAllTasks();
+            return Ok(tasks);
+        }
+
+        [Authorize]
+        [HttpPost("CreateTask")]
+        public async Task<ActionResult<Task>> CreateTask(TaskDto task)
+        {
+            var createdTask = await _tasksService.CreateTask(task);
+            return CreatedAtAction(nameof(GetAllTasks), new { id = createdTask.Id }, createdTask);
+        }
+
+        [Authorize]
+        [HttpPut("UpdateTask/{taskId}")]
+        public async Task<ActionResult<Task>> UpdateTask(int taskId, TaskDto task)
+        {
+            if (taskId != task.Id)
+                return BadRequest("Task Id not the same.");
+
+            var updatedTask = await _tasksService.UpdateTask(task);
+
+            if (updatedTask == null)
+                return NotFound();
+
+            return Ok(updatedTask);
+        }
+
+        [Authorize]
+        [HttpDelete("DeleteTask/{taskId}")]
+        public async Task<ActionResult<bool>> DeleteTask(int taskId)
+        {
+            var success = await _tasksService.DeleteTask(taskId);
+
+            if (!success)
+                return NotFound();
+
+            return true; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using TaskManagementSystemData;
+using TaskManagementSystemRepository;
+using TaskManagementSystemRepository.Interfaces;
+using TaskManagementSystemService;
+using TaskManagementSystemService.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<ITasksService, TasksService>();
+
+builder.Services.AddScoped<ITasksRepository, TasksRepository>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "nikolaFilipovski",
+            ValidAudience = "testApp",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsA32CharacterLongSecretKey!"))
+        };
+    });
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter the token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+// CORS for the Application UI, the React app.
+var corsPolicyName = "AllowReactApp";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicyName, builder =>
+    {
+        builder.WithOrigins("http://localhost:5173") // React app URL
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials(); // This is for the Authorization
+    });
+
+    options.AddPolicy("AllowSwagger", builder =>
+    {
+        builder.WithOrigins("https://localhost:44334") // Swagger UI
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Management API"));
+}
+
+app.UseCors(builder.Environment.IsDevelopment() ? "AllowSwagger" : corsPolicyName);
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
